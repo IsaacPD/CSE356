8b04e4bb57be0b45a38054ea0662203f2c97cbbf
Isaac <isaacpd@yahoo.com>
2019-03-05 22:32:24 -0500

    more logging

--- hw3/routes/index.js
+++ hw3/routes/index.js
@@ -3,27 +3,30 @@ var router = express.Router();
 var amqp = require('amqplib/callback_api');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
 	console.log(req.body);
 	res.render('index', {title: 'Express'});
 });
 
 router.post('/', function (req, res) {
 	console.log(req.body);
-	res.json({status:"OK"});
+	res.json({status: "OK"});
 });
 
 router.post('/listen', function (req, res) {
 	let keys = req.body.keys;
+	console.log(keys);
 	amqp.connect('amqp://localhost', function (err, conn) {
 		conn.createChannel(function (err, ch) {
-			if (err) return res.json({status:"ERROR"});
+			if (err) return res.json({status: "ERROR"});
 			let ex = 'hw3';
-			ch.assertExchange(ex, 'direct', {durable:false});
+			ch.assertExchange(ex, 'direct');
 			ch.assertQueue('excl', {exclusive: true}, function (err, q) {
-				if (err) return res.json({status:"ERROR"});
+				if (err) return res.json({status: "ERROR"});
 				keys.forEach((key) => ch.bindQueue(q.queue, ex, key));
-				ch.consume(q.queue, (msg) => res.json({msg:msg.content.toString()}), {noAck: true})
+				ch.consume(q.queue, function (msg) {
+					res.json({msg: msg.content.toString()});
+				}, {noAck: false})
 			});
 		});
 	});
@@ -31,16 +34,14 @@ router.post('/listen', function (req, res) {
 
 router.post('/speak', function (req, res) {
 	let {key, msg} = req.body;
+	console.log(key, msg);
 	amqp.connect('amqp://localhost', function (err, conn) {
 		conn.createChannel(function (err, ch) {
 			let ex = 'hw3';
-			ch.assertExchange(ex, 'direct', {durable:false});
+			ch.assertExchange(ex, 'direct');
 			ch.publish(ex, key, Buffer.from(msg));
-			res.json({status:"OK"});
+			res.json({status: "OK"});
 		});
-		setTimeout(function () {
-			conn.close();
-		}, 500);
 	});
 });
 
