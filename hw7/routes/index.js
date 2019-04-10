d9de870d115171aa2df5eec92c58088cf53580cb
Isaac <isaacpd@yahoo.com>
2019-04-10 09:57:43 -0400

    create query

--- hw7/routes/index.js
+++ hw7/routes/index.js
@@ -1,5 +1,17 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'ubuntu',
+  password: 'ubuntu',
+  database: 'hw7'
+});
+
+connection.connect();
+
+let query = "SELECT * FROM assists WHERE Club=? AND Pos=? AND A=(SELECT MAX(A) FROM assists WHERE Club=? AND Pos=?)";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,6 +19,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/hw7', function(req, res){
+  let club = req.query.club;
+  let pos = req.query.pos;
+
+  connection.query(query, [club, pos], function(err, result){
+    res.json({result: result});
+    //res.json({club: club, pos: pos, max_assists:, player;, avg_assists:});
+  })
 
 });
 
